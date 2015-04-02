ea813ee14c5f98ba94c35aa59644972182781985
mornrisk <akoshim@gmail.com>
2015-04-02 18:21:04 +0900

    improve server traffic

--- cpanfile
+++ cpanfile
@@ -2,3 +2,4 @@ requires 'Config::PL';
 requires 'Furl';
 requires 'Device::SerialPort';
 requires 'Math::Decimal';
+requires 'Time::Piece';
--- wipi.pl
+++ wipi.pl
@@ -8,22 +8,36 @@ use Device::SerialPort;
 use Math::Decimal qw/is_dec_number dec_canonise/;
 use Time::Piece;
 
-use constant READ_SIZE => 512;
 use utf8;
 binmode STDOUT, ":utf8";
 
 my $config = config_do 'config.pl';
 my $furl   = Furl->new();
 
+my $last_hms       = 0;
+my $last_is_stable = '';
+my $last_weight    = '';
 sub transmit {
     my ($is_stable, $weight) = @_;
+
+    my $t = localtime;
+    my $hms = $t->hour * 10000 + $t->min * 100 + $t->sec;
+    if ($last_is_stable eq $is_stable and $last_weight eq $weight){
+        return if $last_hms == $hms;
+    }
 
     my $res = $furl->post(
         $config->{post_url},
         [],
         [wi_id => $config->{wipi_name}, stable => $is_stable, weight => $weight, ],
     );
-    warn 'weight transmit failed' if $res->code != 200;
+    if ($res->code == 200){
+        $last_is_stable = $is_stable;
+        $last_weight    = $weight;
+        $last_hms       = $hms;
+    }else{
+        warn 'weight transmit failed' if $res->code != 200;
+    }
 }
 
 my $port = Device::SerialPort->new($config->{device}->{port}) or die "can't open serial port";
@@ -38,14 +52,19 @@ $port->read_const_time(0);
 my $buff = '';
 my ($cnt, $reads, $idx);
 my ($is_stable, $weight);
+my ($blocking, $in_bytes, $out_bytes, $errors);
 while (1){
-    ($cnt, $reads) = $port->read(READ_SIZE);
+    ($blocking, $in_bytes, $out_bytes, $errors) = $port->status;
+    next unless $in_bytes;
 
+    ($cnt, $reads) = $port->read($in_bytes);
+    next unless $cnt;
     $buff .= $reads;
+    next if length($buff) < $config->{protocol}->{data_len};
     $idx = rindex($buff, $config->{protocol}->{delimiter});
     next if $idx < $config->{protocol}->{data_len} - 1;
 
-    $port->purge_rx;
+    $port->purge_all;
     $buff = substr($buff, $idx - $config->{protocol}->{data_len} + 1);
     $is_stable = substr($buff, $config->{protocol}->{status_ch_pos}, 1) 
                     eq $config->{protocol}->{stable_ch} ? 1 : 0;
